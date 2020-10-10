a3840d8bd422bcde5196f5f2c6583c20f6ee7c48
Crlwebby <1303965943@qq.com>
2020-10-10 23:12:56 +0800

    Chapter3 accomplished

--- Chapter_3/3.5_later.php
+++ Chapter_3/3.5_later.php
@@ -1,5 +1,5 @@
 <?php
-    /*$products = array(
+    $products = array(
         array('TIR','Tires',100),
         array('OIL','Oil',10),
         array('SPK','Spark Plugs',4)
@@ -9,14 +9,14 @@
             echo '|'.$products[$row][$column];
         }
         echo '|'."\n";
-    }*/
-    $products = array('Tires','Oil','Spark Plugs');
-    sort($products);
+    }
+    //$products = array('Tires','Oil','Spark Plugs');
+    /*sort($products);
 
     foreach($products as $var){
         echo $var."\t";
     }
-    echo "\n";
+    echo "\n";*/
 
     /*function putout(array $array){
         foreach($array as $key => $value){
@@ -25,10 +25,50 @@
         echo "\n";
     }*/
     $prices = array('Tires'=>100,'Oil'=>10, 'Spark Plugs'=>4);
+    foreach($prices as $key => $value){
+        echo $key."=>\t".$value."\t";
+        echo "\n";
+    }
+    echo '==================================='."\n";
     asort($prices);
     //putout($prices);
     foreach($prices as $key => $value){
         echo $key."=>\t".$value."\t";
         echo "\n";
     }
+    echo '==================================='."\n";
+    ksort($prices);
+    foreach($prices as $key => $value){
+        echo $key."=>\t".$value."\t";
+        echo "\n";
+    }
+   // var_dump($products);
+    //array_multisort($products);
+    //var_dump($products);
+
+    $numbers = range(100,10,-2);
+    foreach($numbers as $var){
+        echo $var."\t";
+    }
+    echo "\n";
+    $numbers = array_reverse($numbers);
+    foreach($numbers as $var){
+        echo $var."\t";
+    }
+    reset($numbers);
+    $temp = current($numbers);
+    var_dump($temp);
+    echo "\n";
+
+    function add(&$value,$key,$userdata){
+        $value += $userdata;
+    }
+    array_walk($numbers,'add',5);
+    foreach($numbers as $var){
+        echo $var."\t";
+    }
+
+    $array = array(4,5,1,2,3,1,2,1);
+    $ac = array_count_values($array);
+    var_dump($ac);
 ?>
